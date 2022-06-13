8e142c9cc4a98e0e8ecd690fa88a08948a9f6476
yoshikimasubuchi <yoshixj4@gmail.com>
2022-06-14 06:15:30 +0900

    :white_check_mark: Fix lint

--- src/services/appMention.ts
+++ src/services/appMention.ts
@@ -2,7 +2,7 @@ import type { SayArguments } from '@slack/bolt';
 import { getRandomSlackUserMentionFromText } from '../utils/helper';
 
 export const appMentionService = (text: string): SayArguments => {
-  const textWithoutBotId = text.replace(/<@[a-zA-Z0-9_]+>/, '')
+  const textWithoutBotId = text.replace(/<@[a-zA-Z0-9_]+>/, '');
   const userMention = getRandomSlackUserMentionFromText(textWithoutBotId);
 
   return {
