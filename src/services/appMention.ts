eb864f98cff6daccdd753049cc621e1e5a012e60
yoshikimasubuchi <yoshixj4@gmail.com>
2022-06-13 10:10:40 +0900

    :pencil2: Fix logic

--- src/services/appMention.ts
+++ src/services/appMention.ts
@@ -2,7 +2,8 @@ import type { SayArguments } from '@slack/bolt';
 import { getRandomSlackUserMentionFromText } from '../utils/helper';
 
 export const appMentionService = (text: string): SayArguments => {
-  const userMention = getRandomSlackUserMentionFromText(text);
+  const textWithoutBotId = text.replace(/<@[a-zA-Z0-9_]+>/, '')
+  const userMention = getRandomSlackUserMentionFromText(textWithoutBotId);
 
   return {
     text: `The user is ${userMention}!`,
